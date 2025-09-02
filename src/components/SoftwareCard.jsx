96625ca86d877a48dfa21ceeefc0da3365f761f2
stcgd <31919739+stcgd@users.noreply.github.com>
2025-09-02 20:42:11 +0800

    Update SoftwareCard.jsx

--- src/components/SoftwareCard.jsx
+++ src/components/SoftwareCard.jsx
@@ -19,7 +19,21 @@ const highlightText = (text, query) => {
   );
 };
 
-
+// SVG 下载图标
+const DownloadIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    aria-hidden="true"
+  >
+    <path
+      fillRule="evenodd"
+      d="M12 2.25a.75.75 0 01.75.75v11.69l3.245-3.245a.75.75 0 011.06 1.06l-4.5 4.5a.75.75 0 01-1.06 0l-4.5-4.5a.75.75 0 111.06-1.06l3.245 3.245V3a.75.75 0 01.75-.75zM6 17.25a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H6.75a.75.75 0 01-.75-.75z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
 
 const SoftwareCard = ({ software, query }) => {
   return (
