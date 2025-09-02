1ac4bdd7a67a6057a15b4d149d92ffab96084793
stcgd <31919739+stcgd@users.noreply.github.com>
2025-09-02 18:57:04 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import softwareData from './data/software.json';
 import SoftwareCard from './components/SoftwareCard';
 import './style.css';
@@ -43,13 +43,22 @@ const App = () => {
   };
 
   return (
-    // 注意: 这里不再有条件渲染的 'dark' 类，因为它已经被应用到 body 上了
     <div className="container">
       <header className="header">
         <h1>软件下载导航</h1>
         <p>快捷获取常用软件安装包@Sunway 远程技术支持 4664456</p>
-        <button className="dark-mode-toggle" onClick={toggleDarkMode}>
-          {darkMode ? '☀️ 白天模式' : '🌙 夜间模式'}
+        <button className="dark-mode-toggle" onClick={toggleDarkMode} aria-label="Toggle dark mode">
+          {darkMode ? (
+            // Sun icon
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
+              <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.76 5.86a.75.75 0 010 1.06l-1.5 1.5a.75.75 0 01-1.06-1.06l1.5-1.5a.75.75 0 011.06 0zM2.25 12a.75.75 0 01.75-.75h2.25a.75.75 0 010 1.5H3a.75.75 0 01-.75-.75zM5.86 16.24a.75.75 0 011.06 0l1.5 1.5a.75.75 0 01-1.06 1.06l-1.5-1.5a.75.75 0 010-1.06zM12 18.75a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0v-2.25a.75.75 0 01.75-.75zM16.24 18.14a.75.75 0 01-1.06 0l-1.5-1.5a.75.75 0 011.06-1.06l1.5 1.5a.75.75 0 010 1.06zM18.75 12a.75.75 0 01.75.75h2.25a.75.75 0 010-1.5h-2.25a.75.75 0 01-.75.75zM18.14 7.76a.75.75 0 010-1.06l1.5-1.5a.75.75 0 011.06 1.06l-1.5 1.5a.75.75 0 01-1.06 0zM12 7.5a4.5 4.5 0 100 9 4.5 4.5 0 000-9z" />
+            </svg>
+          ) : (
+            // Moon icon
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
+              <path d="M9.53 2.043a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V2.793a.75.75 0 01.75-.75zM16.47 5.03a.75.75 0 01.061.884l-1.5 2.25a.75.75 0 01-1.144-.763l1.5-2.25a.75.75 0 01.583-.121zM18.75 9.5a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0v-2.25a.75.75 0 01.75-.75zM18.14 14.86a.75.75 0 011.06 0l1.5 1.5a.75.75 0 01-1.06 1.06l-1.5-1.5a.75.75 0 010-1.06zM12 18.75a.75.75 0 01-.75-.75v-2.25a.75.75 0 011.5 0v2.25a.75.75 0 01-.75.75zM5.86 18.14a.75.75 0 01-1.06 0l-1.5-1.5a.75.75 0 011.06-1.06l1.5 1.5a.75.75 0 010 1.06zM2.25 12.5a.75.75 0 01.75-.75h2.25a.75.75 0 010 1.5H3a.75.75 0 01-.75-.75zM5.03 7.53a.75.75 0 01.884-.061l2.25 1.5a.75.75 0 01-.763 1.144l-2.25-1.5a.75.75 0 01-.121-.583zM12 15a3 3 0 100-6 3 3 0 000 6z" />
+            </svg>
+          )}
         </button>
       </header>
 
